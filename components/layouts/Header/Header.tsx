0b60edc60cc83dfc485ead2fe79b6e95cdd4d2a6
Maxim <schubert.walk@yandex.ru>
2024-08-11 12:53:59 +0300

    build: Инициализация проекта

---
+++ components/layouts/Header/Header.tsx
@@ -0,0 +1,7 @@
+interface HeaderProps {
+  className?: string
+}
+
+export const Header = ({ className }: HeaderProps) => (
+  <div className={styles.Header}>Pizdec_Header</div>
+)
---
+++ components/modules/Header/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => (
+  <header className='relative z-[3] bg-dark-blue'>
+    <div className='bg-[#1D2533]'>
+      <button className=''></button>
+    </div>
+  </header>
+)
+
+export default Header
