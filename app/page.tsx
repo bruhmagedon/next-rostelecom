9b9a237928078caaf7e767e0b87f43583ecc7104
Maxim <schubert.walk@yandex.ru>
2024-08-14 23:00:11 +0300

    feat: Настройка главной страница, глобальные стили и её базоый layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-      'pizda'
-      </div>
+    <main>
+      <div>pizda</div>
     </main>
   );
 }
